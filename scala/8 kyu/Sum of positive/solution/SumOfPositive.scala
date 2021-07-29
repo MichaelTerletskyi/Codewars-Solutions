c397f2b5e3bdbeaeb5ee3a3dd3539319029a3980
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-07-29 14:06:36 +0300

    Sum of positive (8 kyu) task has been resolved on Scala language

---
+++ scala/8 kyu/Sum of positive/solution/SumOfPositive.scala
@@ -0,0 +1,3 @@
+object SumOfPositive {
+  def positiveSum(array: Array[Int]): Int = array.filter(d => d > 0).sum
+}
---
+++ scala/8 kyu/Sum of positive/test/ExampleSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatest._
+
+class ExampleSpec extends FlatSpec with Matchers {
+  "positivieSum(Array(2, 2))" should "return 4" in {
+    SumOfPositive.positiveSum(Array(2, 2)) should be(4)
+  }
+
+  "positiveSum(Array(1,-2,3,4,5))" should "return 13" in {
+    SumOfPositive.positiveSum(Array(1, -2, 3, 4, 5)) should be(13)
+  }
+
+  "positiveSum(Array())" should "return 0" in {
+    SumOfPositive.positiveSum(Array()) should be(0)
+  }
+
+  "positiveSum(Array(-1,-2,-3,-4,-5))" should "return 0" in {
+    SumOfPositive.positiveSum(Array(-1, -2, -3, -4, -5)) should be(0)
+  }
+}
