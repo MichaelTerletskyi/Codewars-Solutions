be84dab2b26916ee801ded28cb15207129447b8b
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-07-31 15:01:12 +0300

    Count Odd Numbers below n (8 kyu) task has been resolved on Scala language

---
+++ scala/8 kyu/Count Odd Numbers below n/solution/OddCount.scala
@@ -0,0 +1,3 @@
+object OddCount {
+  def oddCount(num: Long): Long = num / 2
+}
---
+++ scala/8 kyu/Count Odd Numbers below n/test/OddCountSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest._
+
+class OddCountSpec extends FlatSpec with Matchers {
+
+  "oddCount(15)" should "return 7" in {
+    OddCount.oddCount(15) should be(7)
+  }
+
+  "oddCount(15023)" should "return 7511" in {
+    OddCount.oddCount(15023) should be(7511)
+  }
+}
