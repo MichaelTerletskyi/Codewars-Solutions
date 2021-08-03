d38a1669f2f75d72394576f6cf85703d0c320f32
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-03 18:37:41 +0300

    Multiplication table for number (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Multiplication table for number/solution/Kata6.java
@@ -0,0 +1,11 @@
+class Kata6 {
+    public static String multiTable(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 11; i++) {
+            String temp = String.format("%s * %s = %s", i, num, (i * num));
+            if (i != 10) sb.append(temp).append('\n');
+            else sb.append(temp);
+        }
+        return sb.toString();
+    }
+}
