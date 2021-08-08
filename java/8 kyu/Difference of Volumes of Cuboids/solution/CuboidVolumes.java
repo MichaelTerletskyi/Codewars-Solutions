cbed783b888155d57f4d1036185cc45385861d86
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-08 20:25:51 +0300

    Difference of Volumes of Cuboids (8 kyu) task has been resolved on Java language

--- java/8 kyu/Difference of Volumes of Cuboids/solution/CuboidVolumes.java
+++ java/8 kyu/Difference of Volumes of Cuboids/solution/CuboidVolumes.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class CuboidVolumes {
     public static int findDifference(final int[] firstCuboid, final int[] secondCuboid) {
-        return Arrays.stream(firstCuboid).reduce(1, (x, y) -> x * y) - Arrays.stream(secondCuboid).reduce(1, (x, y) -> x * y);
+        return Math.abs(Arrays.stream(firstCuboid).reduce(1, (x, y) -> x * y) - Arrays.stream(secondCuboid).reduce(1, (x, y) -> x * y));
     }
 }
