ee2153e484ac676902b40bbc9261fc26a984eb54
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-09 17:59:48 +0300

    Switch it Up! (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Switch it Up!/solution/SwitchItUp.java
@@ -0,0 +1,5 @@
+public class SwitchItUp {
+    public static String switchItUp(int n) {
+        return new String[]{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}[n];
+    }
+}
---
+++ java/8 kyu/Switch it Up!/test/SwitchItUpTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SwitchItUpTest {
+
+    @Test
+    public void basicTests() {
+        assertEquals("One", SwitchItUp.switchItUp(1));
+        assertEquals("Three", SwitchItUp.switchItUp(3));
+        assertEquals("Five", SwitchItUp.switchItUp(5));
+    }
+}
