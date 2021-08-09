630da996b5bbdc2804d3369e9f06612356c5e937
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-09 16:23:56 +0300

    Find the position! (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Find the position!/solution/FindThePosition.java
@@ -0,0 +1,18 @@
+public class FindThePosition {
+    // My Solution
+    public static String position(char alphabet) {
+        char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int position = 0;
+        System.out.println(chars.length);
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == alphabet) position = ++i;
+        }
+        return String.format("%s: %s", "Position of alphabet", String.valueOf(position));
+    }
+
+//    Best Soltuion
+//    public static String position(char alphabet) {
+//        return "Position of alphabet: " + (alphabet - '`');
+//    }
+}
+
