0bc7b9094ff39a855f124fa9f60dc0d86753f957
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-12 22:57:20 +0300

    Welcome! (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Welcome!/solution/Welcome.java
@@ -0,0 +1,23 @@
+public class Welcome {
+    public static String greet(String language) {
+        java.util.Map<String, String> map = new java.util.HashMap<>();
+        map.put("english", "Welcome");
+        map.put("czech", "Vitejte");
+        map.put("danish", "Velkomst");
+        map.put("dutch", "Welkom");
+        map.put("estonian", "Tere tulemast");
+        map.put("finnish", "Tervetuloa");
+        map.put("flemish", "Welgekomen");
+        map.put("french", "Bienvenue");
+        map.put("german", "Willkommen");
+        map.put("irish", "Failte");
+        map.put("italian", "Benvenuto");
+        map.put("latvian", "Gaidits");
+        map.put("lithuanian", "Laukiamas");
+        map.put("polish", "Witamy");
+        map.put("spanish", "Bienvenido");
+        map.put("swedish", "Valkommen");
+        map.put("welsh", "Croeso");
+        return map.getOrDefault(language, "Welcome");
+    }
+}
