1624ed5ce33e8a7784736693d32a00dedd266ccd
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-14 01:18:51 +0300

    Number toString (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Number toString/solution/ToString.java
@@ -0,0 +1,6 @@
+public class ToString {
+    public static final String a = 123 + "";
+ // public static final String a = String.valueOf(123);
+ // public static final String a = Integer.toString(123);
+ // public static final String a = String.format("%s", 123);
+}
---
+++ java/8 kyu/Number toString/test/ToStringTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ToStringTest {
+
+    @Test
+    public void tests() {
+        assertEquals("Wrong!", ToString.a, "123");
+    }
+}
