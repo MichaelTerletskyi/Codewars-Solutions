cef74c320abac07b406509ef78463325011125e2
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-15 00:21:42 +0300

    Volume of a Cuboid (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Volume of a Cuboid/solution/VolumeOfACuboid.java
@@ -0,0 +1,5 @@
+public class VolumeOfACuboid {
+    public static double getVolumeOfCuboid(double l, double w, double h) {
+        return l * w * h;
+    }
+}
---
+++ java/8 kyu/Volume of a Cuboid/test/VolumeOfACuboidTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VolumeOfACuboidTest {
+    private static final double delta = 0.0001;
+
+    @Test
+    public void examples() {
+        assertEquals(4, VolumeOfACuboid.getVolumeOfCuboid(1, 2, 2), delta);
+        assertEquals(63, VolumeOfACuboid.getVolumeOfCuboid(6.3, 2, 5), delta);
+    }
+}
