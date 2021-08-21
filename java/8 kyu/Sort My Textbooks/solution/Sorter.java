175b43d5a868e066bbd6c85d71be697a3b08ca37
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-22 02:32:20 +0300

    Sort My Textbooks (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Sort My Textbooks/solution/Sorter.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+class Sorter {
+    public static List<String> sort(List<String> textbooks) {
+        java.util.Collections.sort(textbooks);
+        return textbooks;
+    }
+}
