4730a46100fe805896133361c69e36d16f4e3e42
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-22 03:22:34 +0300

    Playing with cubes I (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Playing with cubes I/solution/Cube.java
@@ -0,0 +1,15 @@
+public class Cube {
+    private int side;
+
+    public Cube() {
+
+    }
+
+    public int getSide() {
+        return this.side;
+    }
+
+    public void setSide(int num) {
+        this.side = num;
+    }
+}
