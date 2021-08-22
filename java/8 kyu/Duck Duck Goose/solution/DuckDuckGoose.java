d678305b818ef1145f346c0c7c89c82d7334109e
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-22 21:28:03 +0300

    Duck Duck Goose (8 kyu) task has been resolved on Java language

---
+++ java/8 kyu/Duck Duck Goose/solution/DuckDuckGoose.java
@@ -0,0 +1,9 @@
+public class DuckDuckGoose {
+//    public static String duckDuckGoose(Player[] players, int goose) {
+//        My Solution
+//        return goose % players.length == 0 ? players[players.length - 1].name : players[goose % players.length - 1].name;
+//
+//        Best Solution
+//        return players[(goose - 1) % players.length].name;
+//    }
+}
