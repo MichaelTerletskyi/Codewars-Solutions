91995e4c77888499686636a9e14936eab3b08bc2
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-08-29 12:22:10 +0300

    Happy Birthday (7 kyu) task has been resolved on Java language

---
+++ java/7 kyu/Happy Birthday/test/WrapPresentTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class WrapPresentTest {
+
+    @Test
+    public void test() {
+        assertEquals(162, WrapPresent.wrap(17, 32, 11));
+        assertEquals(124, WrapPresent.wrap(13, 13, 13));
+        assertEquals(32, WrapPresent.wrap(1, 3, 1));
+    }
+}
