261b12354ca674e9ecfead6824716b755193410b
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-09-01 20:16:54 +0300

    Factorial (7 kyu) task has been resolved on Java language

---
+++ java/7 kyu/Factorial/solution/Factorial.java
@@ -0,0 +1,6 @@
+public class Factorial {
+    public int factorial(int n) {
+        if (n < 0 || n > 12) throw new IllegalArgumentException();
+        return n <= 1 ? 1 : n * factorial(n - 1);
+    }
+}
