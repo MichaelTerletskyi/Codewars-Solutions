3a8eccabb00a3069a070ef14708cb783667fb0d1
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2022-06-24 05:36:58 +0300

    pre2 :: ow many numbers III? task has been ? on Java

--- 4 kyu/How many numbers III/solutions/HowManyNumbers.java
+++ 4 kyu/How many numbers III/solutions/HowManyNumbers.java
@@ -14,68 +14,7 @@ public class HowManyNumbers {
 
         long[] arr = new long[numDigits];
         Arrays.fill(arr, 1);
-
-
         int index = arr.length - 1;
-        fillArray(arr, index, sumDigits);
-        long possibleNumbers = 1L;
-
-        long min = getNumber(arr);
-        System.out.println(String.format("%d : ++%d", getNumber(arr), possibleNumbers));
-        index = 0;
-        while (true) {
-            ++possibleNumbers;
-
-            index = lookingForIndex(arr, index);
-            if (index + 1 < arr.length) {
-                if (arr[index] + 1 <= arr[index + 1] - 1) {
-                    arr[index] = ++arr[index];
-                    arr[index + 1] = --arr[index + 1];
-
-                    System.out.println(String.format("%d : ++%d", getNumber(arr), possibleNumbers));
-                } else {
-                    incArray(index, arr, sumDigits);
-                    System.out.println(String.format("%d : ++%d", getNumber(arr), possibleNumbers));
-                }
-            } else {
-                incArray(index, arr, sumDigits);
-                System.out.println(String.format("%d : ++%d", getNumber(arr), possibleNumbers));
-                break;
-            }
-        }
-        long max = getNumber(arr);
-        return List.of(possibleNumbers, min, max);
-    }
-
-    private static int lookingForIndex(long[] arr, int index) {
-        while (index + 1 < arr.length && arr[index] == arr[index + 1]) {
-            ++index;
-        }
-        return index;
-    }
-
-    private static int incArray(int index, long[] arr, int sumDigits) {
-        int tempIndex = index;
-        index = arr.length - 1;
-        if (arr[arr.length - 1 - index] + 1 < 9) {
-            long temp = arr[arr.length - 1 - index];
-            Arrays.fill(arr, temp + 1);
-            sumDigits -= LongStream.of(arr).sum();
-            for (int i = arr.length - 1; i > 0; i--) {
-                long j = arr[i];
-                long diff = Math.min(arr[i] + sumDigits, 9);
-                if (diff - j == 0) {
-                    continue;
-                }
-                arr[i] = diff;
-                sumDigits -= (diff - j);
-            }
-        }
-        index = tempIndex;
-        return index;
-    }
-
-    private static int fillArray(long[] arr, int index, final int sumDigits) {
         int arrMinSum = arr.length;
         while (arrMinSum != sumDigits) {
             if (arr[index] < 9) {
@@ -85,10 +24,48 @@ public class HowManyNumbers {
                 --index;
             }
         }
-        return index;
+
+        long min = getNumber(arr);
+        Arrays.fill(arr, sumDigits / numDigits);
+        long sum = sumDigits - LongStream.of(arr).sum();
+        while (sum != 0) {
+            for (int i = arr.length - 1; i > 0; i--) {
+                arr[i] = ++arr[i];
+                sum -= 1;
+                if (sum == 0) break;
+            }
+        }
+        long max = getNumber(arr);
+
+        long possibleNumbers = 0L;
+        for (long i = max; i >= min; i--) {
+            if (isValid(i, sumDigits)) {
+                ++possibleNumbers;
+            }
+        }
+
+        return List.of(possibleNumbers, min, max);
     }
 
-    public static int getNumber(long[] arr) {
+    private static boolean isValid(long temp, int sumDigits) {
+        long[] array = new long[String.valueOf(temp).length()];
+        int index = array.length - 1;
+        do {
+            long val = temp % 10;
+            array[index] = val;
+            temp /= 10;
+            sumDigits -= val;
+            --index;
+        } while (temp > 0);
+        for (int i = array.length - 1; i > 0; i--) {
+            if (array[i] < array[i - 1]) {
+                return false;
+            }
+        }
+        return sumDigits == 0;
+    }
+
+    private static int getNumber(long[] arr) {
         int number = 0;
         int offset = 1;
         for (int i = arr.length - 1; i >= 0; i--) {
@@ -99,13 +76,13 @@ public class HowManyNumbers {
     }
 
     public static void main(String[] args) {
-        System.out.println(findAll(10, 3)); //8 :: 118, 127, 136, 145, 226, 235, 244, 334
+        System.out.println(findAll(10, 3)); // 8 :: 118, 127, 136, 145, 226, 235, 244, 334
         System.out.println("\n");
-//        System.out.println(findAll(27, 3)); // 1, 999, 999
-//        System.out.println(findAll(84, 4)); // []
-        System.out.println(findAll(35, 6)); // 23 :: 116999, 566666
+        System.out.println(findAll(27, 3)); // 1:: 999, 999
         System.out.println("\n");
-
-
+        System.out.println(findAll(84, 4)); // []
+        System.out.println("\n");
+        System.out.println(findAll(35, 6)); // 123L :: 116999, 566666
+        System.out.println("\n");
     }
 }
