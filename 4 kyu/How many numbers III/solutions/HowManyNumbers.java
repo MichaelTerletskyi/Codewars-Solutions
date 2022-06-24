219090af2455f6a9858635178840146d14139342
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2022-06-24 05:55:29 +0300

    How many numbers III? task has been solved and added on Java

--- 4 kyu/How many numbers III/solutions/HowManyNumbers.java
+++ 4 kyu/How many numbers III/solutions/HowManyNumbers.java
@@ -1,79 +1,101 @@
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.LongStream;
 
 public class HowManyNumbers {
+    private static long possibleNumbersCounter, min, max;
+
     public static List<Long> findAll(final int sumDigits, final int numDigits) {
-        if (9 * numDigits < sumDigits || sumDigits < numDigits) {
-            return List.of();
-        }
-        if (9 * numDigits == sumDigits) {
-            long val = Long.parseLong("9".repeat(numDigits));
-            return List.of(1L, val, val);
-        }
-
-        long[] arr = new long[numDigits];
-        Arrays.fill(arr, 1);
-        int index = arr.length - 1;
-        int arrMinSum = arr.length;
-        while (arrMinSum != sumDigits) {
-            if (arr[index] < 9) {
-                arr[index] = ++arr[index];
-                ++arrMinSum;
-            } else {
-                --index;
-            }
-        }
-
-        long min = getNumber(arr);
-        Arrays.fill(arr, sumDigits / numDigits);
-        long sum = sumDigits - LongStream.of(arr).sum();
-        while (sum != 0) {
-            for (int i = arr.length - 1; i > 0; i--) {
-                arr[i] = ++arr[i];
-                sum -= 1;
-                if (sum == 0) break;
-            }
-        }
-        long max = getNumber(arr);
-
-        long possibleNumbers = 0L;
-        for (long i = max; i >= min; i--) {
-            if (isValid(i, sumDigits)) {
-                ++possibleNumbers;
-            }
-        }
-
-        return List.of(possibleNumbers, min, max);
+        possibleNumbersCounter = 0; min = 0; max = 0;
+        recursiveSearch(0L, 1, sumDigits, numDigits);
+        return possibleNumbersCounter > 0 ? List.of(possibleNumbersCounter, min, max) : List.of();
     }
 
-    private static boolean isValid(long temp, int sumDigits) {
-        long[] array = new long[String.valueOf(temp).length()];
-        int index = array.length - 1;
-        do {
-            long val = temp % 10;
-            array[index] = val;
-            temp /= 10;
-            sumDigits -= val;
-            --index;
-        } while (temp > 0);
-        for (int i = array.length - 1; i > 0; i--) {
-            if (array[i] < array[i - 1]) {
-                return false;
+    private static void recursiveSearch(Long currentNumber, int previousDigit, int sumLeft, int digitsLeft) {
+        if (sumLeft == 0 && digitsLeft == 0) {
+            if (possibleNumbersCounter == 0) {
+                min = currentNumber;
+            }
+            min = min < currentNumber ? min : currentNumber;
+            max = max > currentNumber ? max : currentNumber;
+            ++possibleNumbersCounter;
+        } else if (digitsLeft != 0) {
+            for (int i = previousDigit; i < 10; i++) {
+                recursiveSearch(10 * currentNumber + i, i, sumLeft - i, digitsLeft - 1);
             }
         }
-        return sumDigits == 0;
     }
 
-    private static int getNumber(long[] arr) {
-        int number = 0;
-        int offset = 1;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            number += arr[i] * offset;
-            offset *= 10;
-        }
-        return number;
-    }
+//    My shitty solution, it works, but "Execution Timed Out (16000 ms)"
+//    public static List<Long> findAll(final int sumDigits, final int numDigits) {
+//        if (9 * numDigits < sumDigits || sumDigits < numDigits) {
+//            return List.of();
+//        }
+//        if (9 * numDigits == sumDigits) {
+//            long val = Long.parseLong("9".repeat(numDigits));
+//            return List.of(1L, val, val);
+//        }
+//
+//        long[] arr = new long[numDigits];
+//        Arrays.fill(arr, 1);
+//        int index = arr.length - 1;
+//        int arrMinSum = arr.length;
+//        while (arrMinSum != sumDigits) {
+//            if (arr[index] < 9) {
+//                arr[index] = ++arr[index];
+//                ++arrMinSum;
+//            } else {
+//                --index;
+//            }
+//        }
+//
+//        long min = getNumber(arr);
+//        Arrays.fill(arr, sumDigits / numDigits);
+//        long sum = sumDigits - LongStream.of(arr).sum();
+//        while (sum != 0) {
+//            for (int i = arr.length - 1; i > 0; i--) {
+//                arr[i] = ++arr[i];
+//                sum -= 1;
+//                if (sum == 0) break;
+//            }
+//        }
+//        long max = getNumber(arr);
+//
+//        long possibleNumbers = 0L;
+//        for (long i = max; i >= min; i--) {
+//            if (isValid(i, sumDigits)) {
+//                ++possibleNumbers;
+//            }
+//        }
+//
+//        return List.of(possibleNumbers, min, max);
+//    }
+//
+//    private static boolean isValid(long temp, int sumDigits) {
+//        long[] array = new long[String.valueOf(temp).length()];
+//        int index = array.length - 1;
+//        do {
+//            long val = temp % 10;
+//            array[index] = val;
+//            temp /= 10;
+//            sumDigits -= val;
+//            --index;
+//        } while (temp > 0);
+//        for (int i = array.length - 1; i > 0; i--) {
+//            if (array[i] < array[i - 1]) {
+//                return false;
+//            }
+//        }
+//        return sumDigits == 0;
+//    }
+//
+//    private static int getNumber(long[] arr) {
+//        int number = 0;
+//        int offset = 1;
+//        for (int i = arr.length - 1; i >= 0; i--) {
+//            number += arr[i] * offset;
+//            offset *= 10;
+//        }
+//        return number;
+//    }
 
     public static void main(String[] args) {
         System.out.println(findAll(10, 3)); // 8 :: 118, 127, 136, 145, 226, 235, 244, 334
