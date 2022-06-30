16282f3e521046505d14d16fb83822d581d7acb3
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2022-06-30 10:39:22 +0300

    Matrix Determinant task has been added on Java

--- 4 kyu/Matrix Determinant/solutions/Matrix.java
+++ 4 kyu/Matrix Determinant/solutions/Matrix.java
@@ -1,42 +1,33 @@
 public class Matrix {
     public static int determinant(int[][] matrix) {
-        if (matrix.length == 1) return Math.abs(matrix[0][0]);
-        if (matrix.length == 2) return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-        int determinant = 0;
-        int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            int floor = 0;
-            int frontIndex = i;
-            int backIndex = i;
-            int positive = matrix[0][i];
-            int negative = matrix[0][i];
-            for (int j = 1; j < n; j++) {
-                ++floor;
-                frontIndex = frontIndex + 1 < n ? ++frontIndex : 0;
-                backIndex = backIndex - 1 >= 0 ? --backIndex : n - 1;
-                positive *= matrix[floor][frontIndex];
-                negative *= matrix[floor][backIndex];
-            }
-            determinant += positive;
-            determinant -= negative;
-        }
-        return determinant;
+        return recursiveSearch(matrix, matrix.length);
     }
 
-    public static void main(String[] args) {
-        int[][][] matrix = {
-                {{1}},
-                {{1, 3}, {2, 5}},
-                {{2, 5, 3}, {1, -2, -1}, {1, 3, 4}},
-                {{1, -1, 0, 0}, {2, 3, 1, -1}, {3, -2, 0, 1}, {-2, 2, 1, -3}},
-                {{1, 2, 3, 4}, {5, 0, 2, 8}, {3, 5, 6, 7}, {2, 5, 3, 1}}
-        };
-        int[] expected = {1, -1, -20, -3, 24};
-
-        for (int n = 0; n < expected.length; n++) {
-            int x = expected[n];
-            int determinant = Matrix.determinant(matrix[n]);
-            System.out.println(String.format("Expected = %d, determinant = %d, %s", x, determinant, expected[n] == Matrix.determinant(matrix[n])));
+    private static void getMatrixCoFactor(int[][] matrix, int[][] temp, int index, int n) {
+        int j = 0; int k = 0;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (row != 0 && col != index) {
+                    temp[j][k++] = matrix[row][col];
+                    if (k == n - 1) {
+                        k = 0;
+                        j++;
+                    }
+                }
+            }
         }
+    }
+
+    private static int recursiveSearch(int[][] matrix, int n) {
+        if (n == 1) return matrix[0][0];
+        int[][] temp = new int[n][n];
+        int prefix = 1;
+        int determinant = 0;
+        for (int index = 0; index < n; index++) {
+            getMatrixCoFactor(matrix, temp, index, n);
+            determinant += prefix * matrix[0][index] * recursiveSearch(temp, n - 1);
+            prefix = -prefix;
+        }
+        return determinant;
     }
 }
